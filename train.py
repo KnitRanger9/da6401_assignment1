bb7e0e9d73c54f2d416d1f7111ec0823466b189a
KnitRanger9 <da24m004@smail.iitm.ac.in>
2025-03-03 17:49:08 +0530

    vscode commit

--- train.py
+++ train.py
@@ -17,18 +17,38 @@ X_train = X_train/255
 X_test = X_test/255
 
 class Neuron:
-  def __init__(self, weight, bias, lr):
-    self.w=weight
-    self.b=bias
-    self.eta=lr
+  def __init__(self):
+    self.data=0.0
 
 class Layer(Neuron):
-  def __init__(self, n, n_out):
-    self.n_in = n_in
-    self.n_out = n_out
-    self.w_matrix = np.zeros(n, n_out)
-    self.b_matrix = np.zeros(n, n_out)
+  def __init__(self, n):
+    self.n_in = n
+    self.data = np.zeros(n)
+    self.neurons = [Neuron() for _ in range(n)]
+    for i in range(n):
+      self.data[i] = self.neurons[i].data
 
-  def forward(self, X):
-    out = np.dot(w_matrix.T, X)
-    return out
+class NeuralNetwork:
+  def __init__(self, n_inputs, n_hidden, n_outputs):
+    self.input_layer = Layer(n_inputs)
+    for i in range(n_inputs):
+      self.input_layer.data[i] = self.input_layer.neurons[i].data
+    self.hidden_layer = []
+    for i in range(n_hidden):
+      n = int(input(f"Enter number of neurons in hidden layer {i}: "))
+      self.hidden_layer.append(Layer(n_hidden))
+    self.hidden_layer = Layer(n_hidden)
+    self.output_layer = Layer(n_outputs)
+    self.weights_ih = np.random.rand(n_hidden, n_inputs)
+    self.weights_ho = np.random.rand(n_outputs, n_hidden)
+    self.bias_h = np.random.rand(n_hidden)
+    self.bias_o = np.random.rand(n_outputs)
+    self.lr = 0.1
+
+    def forward(self, inputs):
+      self.input_layer.data = inputs
+      self.hidden_layer.data = np.dot(self.weights_ih, inputs) + self.bias_h
+      self.hidden_layer.data = self.sigmoid(self.hidden_layer.data)
+      self.output_layer.data = np.dot(self.weights_ho, self.hidden_layer.data) + self.bias_o
+      self.output_layer.data = self.sigmoid(self.output_layer.data)
+      return self.output_layer.data
