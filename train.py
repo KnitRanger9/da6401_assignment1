93c66b26275de7b8edfc0e76fd359b07a8b80d0b
KnitRanger9 <da24m004@smail.iitm.ac.in>
2025-03-04 12:28:25 +0530

    vscode commit

--- train.py
+++ train.py
@@ -17,10 +17,6 @@ run = wandb.init(project="fashion_mnist")
 X_train = X_train/255
 X_test = X_test/255
 
-class Neuron:
-  def __init__(self, data =0.0):
-    self.data = data
-
 def visualize_data():
   plt.figure(figsize=(10,10))
   
@@ -30,7 +26,94 @@ def visualize_data():
     plt.title(f"Image {i+1}")
   plt.show()
 
+#Helper function
+def sigmoid(x):
+  f_x = lambda x: 1/(1+np.exp(-x))
+  df_x = lambda x: f_x(x)*(1-f_x(x))
+  return f_x, df_x
+
+def relu(x):
+  f_x = lambda x: np.maximum(0,x)
+  df_x = lambda x: np.where(x>0, 1, 0)
+  return f_x, df_x
+
+def tanh(x):
+  f_x = lambda x: np.tanh(x)
+  df_x = lambda x: 1 - np.tanh(x)**2
+  return f_x, df_x
+
+def sgd(weights, bias, lr, grad_w, grad_b):
+  weights -= lr*grad_w
+  bias -= lr*grad_b
+  return weights, bias
+
+def momentum_gd(weights, bias, lr, grad_w, grad_b, beta, v_w, v_b):
+  v_w = beta*v_w + lr*grad_w
+  v_b = beta*v_b + lr*grad_b
+  weights -= v_w
+  bias -= v_b
+  return weights, bias, v_w, v_b
+
+def nest_acc_gd(weights, bias, lr, grad_w, grad_b, beta, v_w, v_b, gamma, v_w_prev, v_b_prev):
+  v_w = beta*v_w + lr*grad_w
+  v_b = beta*v_b + lr*grad_b
+  weights -= (gamma*v_w_prev + v_w)
+  bias -= (gamma*v_b_prev + v_b)
+  return weights, bias, v_w, v_b, v_w_prev, v_b_prev
+
+def rms_prop(weights, bias, lr, grad_w, grad_b, beta, v_w, v_b, epsilon):
+  v_w = beta*v_w + (1-beta)*(grad_w**2)
+  v_b = beta*v_b + (1-beta)*(grad_b**2)
+  weights -= lr*grad_w/(np.sqrt(v_w) + epsilon)
+  bias -= lr*grad_b/(np.sqrt(v_b) + epsilon)
+  return weights, bias, v_w, v_b
+
+def adam(weights, bias, lr, grad_w, grad_b, beta1, beta2, epsilon, m_w, m_b, v_w, v_b, t):
+  m_w = beta1*m_w + (1-beta1)*grad_w
+  m_b = beta1*m_b + (1-beta1)*grad_b
+  v_w = beta2*v_w + (1-beta2)*(grad_w**2)
+  v_b = beta2*v_b + (1-beta2)*(grad_b**2)
+  m_w_hat = m_w/(1-beta1**t)
+  m_b_hat = m_b/(1-beta1**t)
+  v_w_hat = v_w/(1-beta2**t)
+  v_b_hat = v_b/(1-beta2**t)
+  weights -= lr*m_w_hat/(np.sqrt(v_w_hat) + epsilon)
+  bias -= lr*m_b_hat/(np.sqrt(v_b_hat) + epsilon)
+  return weights, bias, m_w, m_b, v_w, v_b
+
+def nadam(weights, bias, lr, epsilon, beta1, beta2, v_w, v_b, m_w, m_b):
+  m_w_prev = beta1*m_w - (1-beta1)*grad_w
+  m_b_prev = beta1*m_b - (1-beta1)*grad_b
+  v_w_prev = beta2*v_w - (1-beta2)*grad_w**2
+  v_b_prev = beta2*v_b - (1-beta2)*grad_b**2
+  weights -= lr*(m_w_prev/(np.sqrt(v_w_prev) + epsilon) + beta1*(m_w - m_w_prev)/(np.sqrt(v_w) + epsilon))
+  bias -= lr*(m_b_prev/(np.sqrt(v_b_prev) + epsilon) + beta1*(m_b - m_b_prev)/(np.sqrt(v_b) + epsilon))
+
+class Neuron:
+  """
+  meta: A neuron object to represent a single neuron unit in layer
+
+  variables:
+  - data: The output stored by the neuron
+  """
+  def __init__(self, data =0.0):
+    self.data = data
+
 class Layer(Neuron):
+  """
+  meta: A Layer object combining several neurons to form a single layer
+
+  variables:
+  - n_input: Number of neurons in the previous layer
+  - n_output: Number of neurons in the current layer
+  - activation: Activation function for the layer
+  - weights: Weights connecting the previous layer to the current layer
+  - bias: Bias for the layer
+
+  methods:
+  - forward: Forward pass through the layer
+  - backward: Backward pass through the layer to update weights and bias
+  """
   def __init__(self, n_input, n_output, activation, data = 0.0):
     self.n_input = n_input
     self.n_output = n_output
@@ -69,7 +152,7 @@ class NeuralNetwork:
 
     n_input = int(input(f"Enter number of neurons in Input layer: "))
     self.layers.append(Layer(n_input, 0, activation))
-    for i in range(n_layers-2):
+    for i in range(n_layers-1):
       n_output = int(input(f"Enter number of neurons in layer {i+1}: "))
       self.layers.append(Layer(n_input, n_output, activation))
       self.weights.append(self.layers[i].weights)
@@ -81,7 +164,7 @@ class NeuralNetwork:
     self.weights.append(self.layers[-1].weights)
     self.bias.append(self.layers[-1].bias)
 
-    def construct()
+    def construct
 
 
     def modify(self, n_hidden):
@@ -95,3 +178,4 @@ class NeuralNetwork:
         inputs = self.activation(np.dot(self.weights[i], inputs) + self.bias[i])
         self.layers[i].data = inputs
       return inputs
+
